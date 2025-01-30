33085f553e9e01067fd1c2b41ca0b83ba7bda08d
suryaelidanto <suryaelidanto@gmail.com>
2025-01-30 14:55:39 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/Button/Button';
+
+function App() {
+  return (
+    <div>
+      <Button label="Click me" onClick={() => {}} />
+    </div>
+  );
+}
+
+export default App;
