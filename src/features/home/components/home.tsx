3cb56bd1a8e2d1159be3d71dd4b423bcfa3c1e1a
suryaelidanto <suryaelidanto@gmail.com>
2025-02-17 15:53:47 +0700

    feat: implementing basic get threads

--- src/features/home/components/home.tsx
+++ src/features/home/components/home.tsx
@@ -1,15 +1,28 @@
 import { Box } from '@chakra-ui/react';
 import CardThread from './card-thread';
 import CreateThread from './create-thread';
-import { postDatas } from '@/utils/fake-datas/posts';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Thread } from '../types/posts';
 
 export default function Home() {
+  const [threads, setThreads] = useState<Thread[]>([]);
+
+  async function getThreads() {
+    const response = await axios.get('http://localhost:3000/threads');
+    setThreads(response.data as Thread[]);
+  }
+
+  useEffect(() => {
+    getThreads();
+  }, []);
+
   return (
     <Box>
       <CreateThread />
       <Box display={'flex'} flexDirection={'column'} gap={'16px'}>
-        {postDatas.map((postData) => (
-          <CardThread postData={postData} key={postData.id} />
+        {threads?.map((thread) => (
+          <CardThread threadData={thread} key={thread.id} />
         ))}
       </Box>
     </Box>
